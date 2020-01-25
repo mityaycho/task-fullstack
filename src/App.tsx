72d1ed9c5da9288b49a02201ca936426f61b0fc9
mityaycho <mityaycho@gmail.com>
2020-01-25 20:49:09 +0300

    add some changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+const App: React.FC = (props: any) => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div className="number">1</div>
+        <span>{props.value}</span>
 				<button>Counter</button>
       </header>
     </div>
